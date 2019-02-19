5bce3e9ea098fc358fb37b7a92e10d7182fac28a
DESKTOP-TOOTMLU\DuyLongPc <LongTMGCS16297@fpt.edu.vn>
2019-02-19 20:28:18 +0700

    query comple in mongodb

--- index.js
+++ index.js
@@ -27,4 +27,22 @@ async function createCourse() {
   console.log(result);
 }
 
-createCourse();
+async function getCourses() {
+  //eq (equal) //ne (not equal) // gt (greater than)
+  //gte (greater than or equal to) // lt (less than)
+  // lte (less than or equal to) // in //nin (not in)
+  //logical ----- or // and
+
+  const courses = await Course.find()
+    // .find({ price: { $in: [10, 15, 20] } })
+    // .find({ price: { $gte: 10, $lte: 20 } })
+    // .find({ author: "TML" })
+    // .or([{ author: "TML" }, { isPublished: false }])
+    // .and([{ author: "TML" }, { isPublished: true }])
+    .limit(10)
+    .sort({ name: 1 })
+    .select({ name: 1, tags: 1 });
+  console.log(courses);
+}
+
+getCourses();
